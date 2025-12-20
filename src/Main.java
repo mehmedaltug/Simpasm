258da86953ad47cc58390229096c311d87d1540f
mehmedaltug <qbert.qwerty.1@gmail.com>
2025-12-20 09:03:36 +0300

    modularized if and loop additions and made the code more traceable

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,12 @@ public class Main {
     static final String DEFAULT_OUTPUT_NAME = "out.asm";
     static final int MAX_NEXTED_STATEMENTS = 30;
 
-    static Stack<String> statement_stack = new Stack<String>(MAX_NEXTED_STATEMENTS);
+    static Stack<String> statement_stack = new Stack<>(MAX_NEXTED_STATEMENTS);
     static int if_count = 0;
     static int loop_count = 0;
     static int line_count = 0;
 
-    public static String negative_comparator(String comparator){
+    public static String negative_comparator(String comparator) throws Exception{
         return switch (comparator) {
             case "<=" -> "g";
             case ">=" -> "l";
@@ -20,7 +20,7 @@ public class Main {
             case ">" -> "le";
             case "==" -> "ne";
             case "!=" -> "e";
-            default -> null;
+            default -> throw new Exception("Invalid comparator");
         };
     }
 
@@ -28,37 +28,47 @@ public class Main {
         return Arrays.stream(str).filter(x -> !x.isEmpty()).toArray(String[]::new);
     }
 
+    public static void handle_push_err(String push_str, int num, String line){
+        try{statement_stack.push(push_str + num);}catch(Exception e){
+            System.out.println("Error at line " + line_count + ":\n" + line+"\nExceeded Maximum Nested Statements");
+            System.exit(-1);
+        }
+    }
+
+    public static String handle_opposite_err(String original, String line){
+        String opposite = null;
+        try{opposite = negative_comparator(original);}catch(Exception e){
+            System.out.println("Error at line " + line_count + ":\n" + line+"\nInvalid Comparator");
+            System.exit(-1);
+        }
+        return opposite;
+    }
+
+    public static String handle_new_if(String[] array, String line){
+        handle_push_err("if_", if_count, line);
+        String opposite = handle_opposite_err(array[2], line);
+        return String.format("cmp %s, %s\nj%s if_%d", array[1], array[3], opposite, if_count);
+    }
+
+    public static String handle_new_loop(String[] array, String line){
+        handle_push_err("loop_end_", loop_count, line);
+        String opposite = handle_opposite_err(array[2], line);
+        return String.format("loop_%d:\ncmp %s, %s\nj%s loop_end_%d", loop_count, array[1], array[3], opposite, loop_count);
+    }
+
     public static String determine_line(String line){
         String[] split_line = line.trim().split("[ (),]");
         split_line = strip_whitespace(split_line);
         String new_line;
         if(line.trim().isEmpty()) return "\n";
-        if(split_line[0].equals("#"))
+        if(line.trim().toCharArray()[0] == '#')
             new_line = "; "+split_line[1];
         else if(split_line[0].equals("if")) {
-            try{statement_stack.push("if_" + if_count);}catch(Exception e){
-                System.out.println("Error at line " + line_count + ":\n" + line+"\nExceeded Maximum Nested Statements");
-                System.exit(-1);
-            }
-            String opposite = negative_comparator(split_line[2]);
-            if(opposite == null){
-                System.out.println("Error at line " + line_count + ":\n" + line+"\nInvalid Comparator");
-                System.exit(-1);
-            }
-            new_line = String.format("cmp %s, %s\nj%s if_%d", split_line[1], split_line[3], opposite, if_count);
+            new_line = handle_new_if(split_line, line);
             if_count++;
         }
         else if(split_line[0].equals("loop")) {
-            try{statement_stack.push("loop_end_" + loop_count);}catch(Exception e){
-                System.out.println("Error at line " + line_count + ":\n" + line+"\nExceeded Maximum Nested Statements");
-                System.exit(-1);
-            }
-            String opposite = negative_comparator(split_line[2]);
-            if(opposite == null){
-                System.out.println("Error at line " + line_count + ":\n" + line+"\n");
-                System.exit(-1);
-            }
-            new_line = String.format("loop_%d:\ncmp %s, %s\nj%s loop_end_%d", loop_count, split_line[1], split_line[3], opposite, loop_count);
+            new_line = handle_new_loop(split_line, line);
             loop_count++;
         }
         else if(split_line[0].equals("}")){
@@ -102,7 +112,7 @@ public class Main {
         File output_file = new File(output_name);
         if(input_file.exists()) output_file.delete();
         output_file.createNewFile();
-        try(Scanner input = new Scanner(input_file);){
+        try(Scanner input = new Scanner(input_file)){
             while(input.hasNextLine()){
                 String line = input.nextLine();
                 line_count++;
