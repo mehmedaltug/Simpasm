539a1a78c823fc52f7349da187e7d1fae5a17a65
mehmedaltug <qbert.qwerty.1@gmail.com>
2025-12-23 16:00:00 +0300

    fixed minor inconvenience

--- jpackage-win.bat
+++ jpackage-win.bat
@@ -1,8 +1,9 @@
+@echo off
 set VERSION=
 for /f "tokens=2 delims=: " %%i in ('type src\META-INF\MANIFEST.MF ^| findstr /i /c:"Implementation"') do (
     set VERSION=%%i
 )
-
+echo Building the executable...
 jpackage --name simpasm --app-version %VERSION% --main-jar out\artifacts\Simpasm_java_jar\Simpasm-java.jar --input . --main-class Main --dest out\artifacts\Simpasm_java_jar\
 
 echo Program executable is created. After installing, dont forget to add it to the user PATH!
